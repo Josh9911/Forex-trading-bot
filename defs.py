95d27d5eac2a1c009f46e0861cb258e394a62bba
josh9911 <thejoshwhowah@gmail.com>
2022-12-31 02:39:48 +0800

    added defs and sample code

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "8a17c3776b2c278c3b9b1e02c80f7164-868c65ea03e1424b0e1cf79de37a1512"
+ACCOUNT_ID = "101-003-20692849-001" 
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
